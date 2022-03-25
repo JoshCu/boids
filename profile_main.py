4eda4c9d2577bd00d2f8ab87f91e79b51fa409b9
JoshCu <Josh.Cu@gmail.com>
2022-03-25 17:40:23 +0000

    add file to run for profiling

---
+++ profile_main.py
@@ -0,0 +1,24 @@
+import numpy as np
+from boid import Boid
+
+width = 800
+height = 800
+bird_size = 10
+flock = [
+    Boid(np.random.randint(0, width),np.random.randint(0, height), width, height, bird_size)
+    for _ in range(50)
+]
+
+def loop():
+    for boid in flock:
+        boid.out_of_bounds()
+        boid.apply_behaviour(flock)
+        boid.update()
+        #boid.show_vision()
+        #boid.show()
+
+
+counter = 0
+while counter < 200:
+    counter += 1
+    loop()
